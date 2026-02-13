dc33c7aacf310b9cfe661789e9b85a76f556e471
Andrea Pozzetti <andrea@pozzetti.it>
2026-02-14 00:49:12 +0100

    deleted server

--- __init__.py
+++ __init__.py
@@ -1,9 +1,5 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-try:
-    from . import sam3_server
-except Exception:
-    pass
 
 WEB_DIRECTORY = "./web"
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
